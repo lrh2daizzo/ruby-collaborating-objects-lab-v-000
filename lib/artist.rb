a735636eacd5c52667fe14f54a2f193a1dae330e
Luis Herrera <herrera.lrh@gmail.com>
2020-03-26 21:49:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,10 +23,16 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if self.all.select {|artist| artist.name == name}
-    else
-      self.new(name).tap {|artist| artist}
+      self.find(name) ? self.find(name) : self.create(name)
     end
-  end
+
+    def self.find(name)
+      self.all.find {|artist| artist.name == name }
+    end
+
+
+    def self.create(name)
+      self.new(name).tap {|artist| artist.save}
+    end
 
 end
