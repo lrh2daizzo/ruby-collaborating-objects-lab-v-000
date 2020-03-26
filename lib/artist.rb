11e1b75c761645e833a20cabf2337c890aacc7d4
Luis Herrera <herrera.lrh@gmail.com>
2020-03-26 21:41:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,16 @@
 class Artist
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def save
+    @@all << self
+  end
 end
