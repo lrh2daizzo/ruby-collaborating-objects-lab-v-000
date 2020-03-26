b79fb512cd620f9a898645c7c5c84979a282a252
Luis Herrera <herrera.lrh@gmail.com>
2020-03-26 23:12:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,11 @@ class Song
 
   def self.new_by_filename(filename)
     parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1]
+    genre_s = parts[2].gsub(".mp3","")
+
+    
     binding.pry
   end
 end
