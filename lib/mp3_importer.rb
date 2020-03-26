e6d7bfaea5b048c10ac6e1a9258752181d14d65b
Luis Herrera <herrera.lrh@gmail.com>
2020-03-26 23:18:07 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,4 +9,8 @@ class MP3Importer
     @files ||= Dir.glob("#{path}/*.mp3").collect { |file| file.gsub("#{path}/","") }
   end
 
+  def import
+    files.each {|file| Song.new_by_filename(file)}
+  end
+  
 end
