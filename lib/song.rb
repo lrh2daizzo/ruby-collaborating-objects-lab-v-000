28d55695046d9a94c5d9c00ed9211814f2841f36
Luis Herrera <herrera.lrh@gmail.com>
2020-03-26 23:16:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,11 @@ class Song
     @@all
   end
 
+  def artist_name=(name)
+   self.artist = Artist.find_or_create_by_name(name)
+   artist.add_song(self)
+ end
+ 
   def self.new_by_filename(filename)
     parts = filename.split(" - ")
     artist_name = parts[0]
